6f16d6f36fcdd5fcae56543917ea9438261a75a8
Ben Greear <greearb@candelatech.com>
2024-06-28 08:29:02 -0700

    mt7996: Enable puncturing support.
    Ported mostly from mtk owrt feed patch, looks like I had some older
    version of this code in there already, so that is updated.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -444,6 +444,12 @@ static int mt7996_config(struct ieee80211_hw *hw, u32 changed)
 		ret = mt76_update_channel(phy->mt76);
 		if (ret)
 			return ret;
+
+		ret = mt7996_mcu_set_pp_en(phy, PP_USR_MODE,
+					   phy->mt76->chandef.punctured);
+		if (ret)
+			return ret;
+
 #if 0
 		if (!mt76_testmode_enabled(phy->mt76) && !phy->mt76->test.bf_en) {
 			ret = mt7996_mcu_edcca_enable(phy, true);
