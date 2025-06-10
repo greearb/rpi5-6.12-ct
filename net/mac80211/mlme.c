0320665a8e07c23f26c9e813fb913a2ab16718f3
Rory Little <rory@candelatech.com>
2025-06-10 14:53:41 -0700

    wifi: mac80211: Fix allowing HE and EHT to operate on 2.4 GHz
    This also simplifies the logic a bit. There are early exit conditions
    later which will limit HT from reporting an invalid mode.
    
    Fixes: c0f6921f25c9 ("wifi: mac80211: Only limit to HT if STA is 2.4 or 5 GHz.")
    Signed-off-by: Rory Little <rory@candelatech.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -5346,7 +5346,6 @@ ieee80211_determine_our_sta_mode(struct ieee80211_sub_if_data *sdata,
 				 struct ieee80211_conn_settings *conn)
 {
 	struct ieee80211_sta_ht_cap sta_ht_cap = sband->ht_cap;
-	bool is_2ghz = sband->band == NL80211_BAND_2GHZ;
 	bool is_5ghz = sband->band == NL80211_BAND_5GHZ;
 	bool is_6ghz = sband->band == NL80211_BAND_6GHZ;
 	const struct ieee80211_sta_he_cap *he_cap;
@@ -5424,20 +5423,14 @@ ieee80211_determine_our_sta_mode(struct ieee80211_sub_if_data *sdata,
 		goto out;
 	}
 
-	if (!vht_cap.vht_supported && is_2ghz) {
-		mlme_link_id_dbg(sdata, link_id,
-				 "no VHT support on 2.4 GHz, limiting to HT\n");
-		goto out;
-	}
-
 	/* VHT - if we have - is fine, including 80 MHz, check 160 below again */
-	if (sband->band != NL80211_BAND_2GHZ) {
-		conn->bw_limit = IEEE80211_CONN_BW_LIMIT_160;
-	}
-	else {
+	if (vht_cap.vht_supported) {
+		conn->mode = IEEE80211_CONN_MODE_VHT;
 		conn->bw_limit = IEEE80211_CONN_BW_LIMIT_40;
 	}
-	conn->mode = IEEE80211_CONN_MODE_VHT;
+
+	if (sband->band != NL80211_BAND_2GHZ)
+		conn->bw_limit = IEEE80211_CONN_BW_LIMIT_160;
 
 	if (is_5ghz &&
 	    !(vht_cap.cap & (IEEE80211_VHT_CAP_SUPP_CHAN_WIDTH_160MHZ |
