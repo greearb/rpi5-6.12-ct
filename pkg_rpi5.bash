6dd2eb872adada422a5efdb87585f70b41c43c41
Ben Greear <greearb@candelatech.com>
2025-12-01 13:44:29 -0800

    pkg:  Check for command failures.

--- pkg_rpi5.bash
+++ pkg_rpi5.bash
@@ -12,13 +12,17 @@ mkdir $RPI_BOOT/firmware
 mkdir $RPI_BOOT/overlays
 
 #set -x
-make -j8 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
-sudo env PATH=$PATH make -j12 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$RPI_ROOT modules_install
-sudo cp arch/arm64/boot/Image $RPI_BOOT/firmware/$KERNEL.img
-sudo cp arch/arm64/boot/dts/broadcom/*.dtb $RPI_BOOT/
-sudo cp arch/arm64/boot/dts/overlays/*.dtb* $RPI_BOOT/overlays/
-sudo cp arch/arm64/boot/dts/overlays/README $RPI_BOOT/overlays/
-sudo cp .config $RPI_BOOT/config-6.12.33-v8-16k-ct+
+make -j8 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs || exit 1
+sudo env PATH=$PATH make -j12 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$RPI_ROOT modules_install || exit 1
+sudo cp arch/arm64/boot/Image $RPI_BOOT/firmware/$KERNEL.img || exit 1
+sudo cp arch/arm64/boot/dts/broadcom/*.dtb $RPI_BOOT/ || exit 1
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* $RPI_BOOT/overlays/ || exit 1
+sudo cp arch/arm64/boot/dts/overlays/README $RPI_BOOT/overlays/ || exit 1
+sudo cp .config $RPI_BOOT/config-6.12.33-v8-16k-ct+ || exit 1
+
+mkdir -p $RPI_BOOT/lib/firmware/mediatek || exit 1
+cp -ar ~/git/linux-firmware/mediatek/* $RPI_BOOT/lib/firmware/mediatek/ || exit 1
+cp -ar ~/git/linux-firmware/intel/iwlwifi/* $RPI_BOOT/lib/firmware/ || exit 1
 
 cd $RPI_ROOT/
 tar -cvzf ../$RPI_TGZ *
